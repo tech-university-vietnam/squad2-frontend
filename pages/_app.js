d3e53180ea6e9b362e30c96c5081335a8d7b9aa1
Thomas Hoang <megatunger@gmail.com>
2022-08-17 14:46:09 +0700

    Update Structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import "../styles/globals.css";
 import { Provider } from "react-redux";
 import { globalStore } from "../src/store/store";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1AB65C",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={globalStore}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Provider>
   );
 }
