d83aa7d47a5b588b1ce1fed9cfb1488a8f6f88d2
Trang Nguyen <trangnguyen.281198@gmail.com>
2022-09-06 08:34:45 +0700

    feat: add booking list

--- pages/bookings/index.js
+++ pages/bookings/index.js
@@ -1,24 +1,51 @@
-import useBookings from "../../src/services/useBookings";
+import { Avatar, Box, IconButton, Typography } from "@mui/material";
 import Link from "next/link";
+import SearchIcon from "@mui/icons-material/Search";
 import routes from "../../src/config/routes";
+import { ThemeColor } from "../../src/config/constants";
+import BookingCard from "../../src/components/BookingCard";
+import useBookings from "../../src/services/useBookings";
 
 const BookingPage = () => {
   const { data } = useBookings();
-  const bookings = data?.bookings || [];
   return (
-    <div>
-      {bookings.map((booking) => {
-        return (
-          <div key={`booking_${booking.id}`}>
-            <Link href={routes.booking_detail(booking.id)}>
-              <a>
-                {booking.id} - {booking.hotel.name}
-              </a>
-            </Link>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        pb={2}
+        pt={3}
+        px={2}
+        sx={{
+          position: "fixed",
+          top: 0,
+          right: 0,
+          left: 0,
+          bgcolor: "white",
+          zIndex: 999,
+        }}
+      >
+        <Box display="flex">
+          <Avatar
+            sx={{ bgcolor: ThemeColor.primary, width: 40, height: 40, mr: 1.5 }}
+            variant="rounded"
+            src="/logo.png"
+          />
+          <Typography variant="h5">My Booking</Typography>
+        </Box>
+        <Link href={routes.search}>
+          <IconButton aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Link>
+      </Box>
+      <Box p={2} pt={10} pb={8}>
+        {data &&
+          data.bookings.map((booking) => (
+            <BookingCard booking={booking} key={booking.id} />
+          ))}
+      </Box>
+    </>
   );
 };
 
---
+++ src/components/BookingCard/index.js
@@ -0,0 +1,72 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Chip,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import routes from "../../config/routes";
+import Link from "next/link";
+import { ThemeColor } from "../../config/constants";
+
+const BookingCard = ({ booking, ...rest }) => {
+  return (
+    <Card
+      sx={{
+        // display: "flex",
+        borderRadius: 2,
+        alignItems: "center",
+        mb: 2,
+      }}
+      elevation={12}
+      {...rest}
+    >
+      <Box display="flex">
+        <CardMedia
+          component="img"
+          image={booking.hotel.images[0]}
+          alt="Hotel image"
+          sx={{ width: 86, height: 86, my: 2, ml: 2, borderRadius: 2 }}
+        />
+
+        <CardContent>
+          <Typography variant="h6" fontWeight={700} color="black">
+            {booking.hotel.name}
+          </Typography>
+          <Typography variant="body1">{booking.hotel.address}</Typography>
+          <Chip
+            label="Created"
+            size="small"
+            sx={{
+              bgcolor: "#e6f5ec",
+              color: ThemeColor.primary,
+              borderRadius: 2,
+              fontSize: 10,
+            }}
+          />
+        </CardContent>
+      </Box>
+      <Link href={routes.booking_detail(booking.id)}>
+        <Button
+          sx={{
+            color: "white",
+            height: 36,
+            width: "50%",
+            float: "right",
+            mb: 1.5,
+            mr: 1.5,
+          }}
+          size="small"
+          variant="contained"
+        >
+          View Tiket
+        </Button>
+      </Link>
+    </Card>
+  );
+};
+
+export default BookingCard;
